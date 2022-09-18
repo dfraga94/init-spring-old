25e2fc76598c34e1cfaf6c408b7e8df16cbf1128
Darwin Fraga <darwinsvladi@hotmail.com>
2022-09-18 07:47:30 -0500

    Creando el primer cotrolador

---
+++ src/main/java/ec/edu/itsqmet/controller/ClientController.java
@@ -0,0 +1,15 @@
+package ec.edu.itsqmet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+	@GetMapping("/saludar")
+	public String saludar() {
+		return "Hellow world";
+	}
+}
